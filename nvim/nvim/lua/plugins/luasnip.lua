359b19585594e6c6a064dc1fc4807d437c437d68
zine0 <zine.xlws@gmail.com>
2024-04-29 17:00:08 +0800

    updata

--- nvim/nvim/lua/plugins/luasnip.lua
+++
@@ -1,2 +0,0 @@
-return {"L3MON4D3/LuaSnip",
-}
--- nvim/nvim/lua/plugins/nvim-lspconfig.lua
+++
@@ -1,9 +0,0 @@
-return {{"neovim/nvim-lspconfig",
-	config = function()
-		local lspconfig = require("lspconfig")
-		lspconfig["lua_ls"].setup({})
-		lspconfig["clangd"].setup({})
-		lspconfig["pyright"].setup({})
-        lspconfig["cmake"].setup({})
-	end
-}}
